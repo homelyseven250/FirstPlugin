c480d2ee132d6d90c85287881858459a700a5208
homelyseven250 <61860079+homelyseven250@users.noreply.github.com>
2020-08-20 10:35:46 +0100

    Add hello command

--- src/main/java/io/github/homelyseven250/firstplugin/FirstPlugin.java
+++ src/main/java/io/github/homelyseven250/firstplugin/FirstPlugin.java
@@ -6,6 +6,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
 public final class FirstPlugin extends JavaPlugin {
 
     @Override
@@ -21,21 +22,15 @@ public final class FirstPlugin extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (label.equalsIgnoreCase("hello")){
+        if (label.equalsIgnoreCase("hello")) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
-                if (player.hasPermission("hello.use")) {
-                    player.sendMessage(ChatColor.AQUA + "Hello!");
-                    return true;
-                }
-            player.sendMessage(ChatColor.RED + "You do not have permission!");
+                player.sendMessage(ChatColor.RED + "Hi!");
+                return true;
             }
-            else {
-                //console
-                sender.sendMessage("Hey console!");
-            }
-            return true;
         }
+
+
         return false;
     }
 }
